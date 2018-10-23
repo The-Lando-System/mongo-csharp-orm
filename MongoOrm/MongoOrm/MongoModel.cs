4d099bb2a97026d09f007287d6a1600cd5f87bac
Matt <matt.voget@gmail.com>
2018-10-22 20:41:45 -0600

    Initial commit

---
+++ MongoOrm/MongoOrm/MongoModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Runtime.Serialization;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace MongoOrm
+{
+    [DataContract]
+    public class MongoModel
+    {
+        [DataMember]
+        [BsonId]
+        public string Id { get; set; }
+
+        [DataMember]
+        public DateTime LastModified { get; set; }
+    }
+}
